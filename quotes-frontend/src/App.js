48ba051069f343a8d7cc29201f5118c417ea60a5
ironkamosi <alintonchambers@gmail.com>
2021-04-21 16:28:41 +0200

    created react-app

---
+++ quotes-frontend/src/App.js
@@ -0,0 +1,27 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [quotesData, setQuotesData] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3001/quotes/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setQuotesData(data);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <header className="App-header">
+          <p>{quotesData.quote}</p>
+          <h5>{quotesData.author}</h5>
+        </header>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- server.js
+++ server.js
@@ -3,11 +3,12 @@
 
 //load the 'express' module which makes writing webservers easy
 const express = require("express");
+const cors = require("cors")
 const app = express();
-
 //load the quotes JSON
 const quotes = require("./quotes.json");
-
+app.use(express.static("quotes-frontend"));
+app.use(cors())
 // Now register handlers for some routes:
 //   /                  - Return some helpful welcome info (text)
 //   /quotes            - Should return all quotes (json)
@@ -15,8 +16,14 @@ const quotes = require("./quotes.json");
 
 //START OF YOUR CODE...
 app.get("/", function (request, response) {
-  const message = `<h1> Hello welcome to my quotes api</h1>
-  <h2>List of content</h2>
+  const message = `<h1> Hello welcome to my inspirational quotes api</h1>
+  <h2>List of contents</h2>
+  <ul>
+  <h3>Level 1 access the routes for the quotes<h3/>
+  <br>
+  <li>/quotes -returns ALL of the quotes, as JSON.</li>
+  <li>/quotes/random - returns ONE of the quotes, picked differently at random each time it is requested.</li>
+  </ul>
   `;
   response.send(message);
 });
@@ -31,9 +38,14 @@ app.get("/quotes/random", function (request, response) {
 
 app.get("/quotes/search", function (req, res) {
   let term = req.query.term.toLowerCase();
-  res.send(searchQuery(term,quotes));
+  res.send(searchQuery(term, quotes));
 });
 
+app.get("/echo", function (req, res) {
+  let term = req.query.term.toLowerCase();
+
+  res.send(`Your term is ${term}`);
+});
 function searchQuery(word, quotes) {
   let filteredResult = quotes.filter(function (element) {
     if (
@@ -43,18 +55,15 @@ function searchQuery(word, quotes) {
       return true;
     }
   });
-  return  filteredResult;
+  return filteredResult;
 
+  //Start our server so that it listens for HTTP requests!
+  // const listener = app.listen(process.env.PORT, function () {
+}
 
 function pickFromArray(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
-
-//Start our server so that it listens for HTTP requests!
-// const listener = app.listen(process.env.PORT, function () {
-//   console.log("Your app is l
-}
-
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
